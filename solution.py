b8a92a6fdfa6926500789b36454c7fc25c490ba8
DesJC <jcd9930@hotmail.ca>
2021-12-07 22:46:29 -0500

    Implement compute_loss_and_accuracy [test4]

--- solution.py
+++ solution.py
@@ -225,7 +225,7 @@ class Trainer:
     def compute_loss_and_accuracy(self, X: torch.Tensor, y: torch.Tensor) -> Tuple[torch.Tensor, float]:
         # Predictions given by network with current weights
         pretty_print('X len', len(X))
-        predictions = self.network(X).clip(self.epsilon, 1 - self.epsilon)
+        predictions = self.network(X).log()#.clip(self.epsilon, 1 - self.epsilon)
         pretty_print_list('Predictions', predictions)
         pretty_print_list('Ys', y)
 
@@ -237,6 +237,7 @@ class Trainer:
         loss = 0
         for actual, expected in zip(predictions, y_choices):
             loss += torch.log(torch.exp(actual[expected]) / torch.exp(actual).sum())
+        loss /= len(X)
         pretty_print('Loss', loss)
 
         # loss_fn = nn.NLLLoss()
